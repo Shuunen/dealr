cfc52fa97b9bbda93e63e7e8fe1fa3f7573dfaf5
Romain Racamier-Lafon <romain.racamier@gmail.com>
2017-11-03 21:01:06 +0100

    project base

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true
+    },
+    globals: {
+        sys: true,
+        alertify: true
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2017
+    },
+    rules: {
+        indent: [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        quotes: [
+            'error',
+            'single'
+        ],
+        semi: [
+            'error',
+            'never'
+        ],
+        'no-console': 0
+    }
+};
